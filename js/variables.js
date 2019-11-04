7f07502f860158f26f9239891cf4c6fd877eab89
Ariel Martin <arielmartin80@gmail.com>
2019-11-04 20:11:06 -0300

    datos del formulario
    se pasaron los datos del fomulario a variables js

--- js/variables.js
+++ js/variables.js
@@ -7,6 +7,20 @@ var youtube = "https://www.instagram.com/credivi/"
 var whatsapp = "https://api.whatsapp.com/send?phone=5491123273372&text=Podras colocar tu mensaje aquí"
 var mailto = "mailto:credivi.marketing@gmail.com"
 
+
+//Fomulario
+var action = "https://docs.google.com/forms/d/e/1FAIpQLSesR4Hshf_ZVGerE1tbbI5tZomHX79IvsJ8B1tfwTjIsXjqeg/formResponse";
+var name = "entry.1094151350";
+var emailAddress = "emailAddress";
+var message = "entry.46917182";
+
+
+
+/******************************************/
+/***********  IMPLEMNTACION  **************/
+/******************************************/
+
+//redes
 $(document).ready(function() {
   $("#titulo1").text(titulo1);
    $(".facebook").attr("href", facebook);
@@ -14,4 +28,12 @@ $(document).ready(function() {
    $(".youtube").attr("href", youtube);
    $(".whatsapp").attr("href", whatsapp);
    $(".mailto").attr("href", mailto)
+});
+
+//Fomulario
+$(document).ready(function() {
+   $("#form").attr("action",action);
+   $("#name").attr("name",name);
+   $("#emailAddress").attr("name",emailAddress);
+   $("#message").attr("name",message);
 });
